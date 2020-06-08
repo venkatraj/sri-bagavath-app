c551e0b7515ea4fab2f6992b439e308e0d1e9aee
Venkat <venkat@codinggeek.com>
2020-06-08 17:42:42 +0530

    contact page

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,14 +1,58 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Button, View, Text, StyleSheet, Linking } from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
 
 const ContactScreen = (props) => {
   return (
     <View>
-      <Text>ContactScreen</Text>
+      <Card style={styles.cardItem}>
+        <Card.Content>
+          <Title>Our Office Location</Title>
+          <Paragraph>
+            31, Ramalingasamy Street, Ammapat, Salem - 636003, Tamil Nadu, India
+          </Paragraph>
+        </Card.Content>
+      </Card>
+      <Card style={styles.cardItem}>
+        <Card.Content>
+          <Title>Contact Number</Title>
+          <Paragraph>Jeevamani +91-9789165555</Paragraph>
+          <Paragraph>V.A.P Saravanan +91- 99942-05880</Paragraph>
+        </Card.Content>
+      </Card>
+      <Card style={styles.cardItem}>
+        <Card.Content>
+          <Title>Email Address</Title>
+          <Paragraph>sribagavathmission@gmail.com</Paragraph>
+          <View style={styles.fixToText}>
+            <Button
+              onPress={() =>
+                Linking.openURL('mailto:sribagavathmission@gmail.com')
+              }
+              title="Send Message"
+            />
+          </View>
+        </Card.Content>
+      </Card>
+      <Card style={styles.cardItem}>
+        <Card.Content>
+          <Title>Map</Title>
+          <Paragraph>TODO</Paragraph>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  cardItem: {
+    marginBottom: 20,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 15,
+  },
+});
 
 export default ContactScreen;
