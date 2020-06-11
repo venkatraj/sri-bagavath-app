4009439a9969b381182858b4c51d3078ab7554c7
Venkat <venkat@codinggeek.com>
2020-06-11 11:21:50 +0530

    price field to events model

--- models/Event.js
+++ models/Event.js
@@ -1,9 +1,10 @@
 class Event {
-  constructor(id, name, host, venue, date) {
+  constructor(id, name, host, venue, price, date) {
     this.id = id;
     this.name = name;
     this.host = host;
     this.venue = venue;
+    this.price = price;
     this.date = date;
   }
 }
