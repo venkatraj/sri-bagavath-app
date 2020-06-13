0259bb9a146521697b71e856d3266d3866760266
Venkat <venkat@codinggeek.com>
2020-06-13 11:49:21 +0530

    firebase storage interaction success

--- components/admin/EBookForm.js
+++ components/admin/EBookForm.js
@@ -4,18 +4,78 @@ import { Button, TextInput, HelperText, Switch } from 'react-native-paper';
 import { useSelector, useDispatch } from 'react-redux';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import * as DocumentPicker from 'expo-document-picker';
 
 import getEBook from '../../utils/getEBook';
 import { addEBook, editEBook } from '../../store/actions/ebooks';
+import { firebase } from '../../firebase/firebase';
 
 const EBookForm = (props) => {
   const { id, onSubmitHandler } = props;
   const ebooks = useSelector((state) => state.ebooks);
   const dispatch = useDispatch();
-  let ebook;
+  let ebook, fileName;
   if (id) {
     ebook = getEBook(id, ebooks);
   }
+
+  const uriToBlob = (uri) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        // return the blob
+        resolve(xhr.response);
+      };
+
+      xhr.onerror = function () {
+        // something went wrong
+        reject(new Error('uriToBlob failed'));
+      };
+      // this helps us get a blob
+      xhr.responseType = 'blob';
+      xhr.open('GET', uri, true);
+
+      xhr.send(null);
+    });
+  };
+
+  const uploadToFirebase = (blob) => {
+    return new Promise((resolve, reject) => {
+      var storageRef = firebase.storage().ref();
+      storageRef
+        .child(`ebooks/${fileName}`)
+        .put(blob, {
+          contentType: 'application/pdf',
+        })
+        .then((snapshot) => {
+          blob.close();
+          resolve(snapshot);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  const chooseEBook = () => {
+    DocumentPicker.getDocumentAsync()
+      .then((result) => {
+        if (result.type !== 'cancel') {
+          const { uri, name, size } = result;
+          fileName = name;
+          return uriToBlob(uri);
+        }
+      })
+      .then((blob) => {
+        return uploadToFirebase(blob);
+      })
+      .then((snapshot) => {
+        console.log('File uploaded!');
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
 
   const onSubmit = (values) => {
     Keyboard.dismiss();
@@ -71,6 +131,7 @@ const EBookForm = (props) => {
             multiline
             numberOfLines={3}
           />
+          <Button onPress={chooseEBook}>Choose EBook</Button>
           <Button disabled={!isValid} onPress={handleSubmit}>
             Submit
           </Button>
