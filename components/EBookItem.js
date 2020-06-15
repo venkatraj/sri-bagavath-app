ddc400b02161ffcd7e1fafcdc13f9cf10f845486
Venkat <venkat@codinggeek.com>
2020-06-15 17:23:22 +0530

    ebook download feature

--- components/EBookItem.js
+++ components/EBookItem.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Card, Button, Title, Paragraph } from 'react-native-paper';
+import * as FileSystem from 'expo-file-system';
+import * as MediaLibrary from 'expo-media-library';
 
 import defaultStyles from '../theme/defaultStyles';
 
 const EBookItem = (props) => {
-  const { title, url, description } = props.ebookData;
+  const { title, fileName, downloadUrl, description } = props.ebookData;
+
+  const downloadEBook = async () => {
+    try {
+      const { status, uri } = await FileSystem.downloadAsync(
+        downloadUrl,
+        FileSystem.documentDirectory + `${fileName}` // `${fileName}.pdf`
+      );
+      if (status === 200) {
+        let permission = await MediaLibrary.getPermissionsAsync();
+        if (!permission.granted) {
+          permission = await MediaLibrary.requestPermissionsAsync();
+        }
+        if (permission.granted) {
+          console.log('Now we can save');
+          const asset = await MediaLibrary.createAssetAsync(uri);
+          const album = await MediaLibrary.createAlbumAsync(
+            'Sri Bagavath',
+            asset
+          );
+          console.log(album, 'Should show a snack bar');
+        } else {
+          console.log('show snack bar not saved message');
+        }
+      } else {
+        throw Error('File download not successful');
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <Card style={defaultStyles.item}>
@@ -13,7 +45,7 @@ const EBookItem = (props) => {
         <Title style={defaultStyles.centeredText}>{title}</Title>
         <Paragraph style={defaultStyles.centeredText}>{description}</Paragraph>
         <Card.Actions style={defaultStyles.centered}>
-          <Button onPress={() => {}}>Download</Button>
+          <Button onPress={downloadEBook}>Download</Button>
         </Card.Actions>
       </Card.Content>
     </Card>
